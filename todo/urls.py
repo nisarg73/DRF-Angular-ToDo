5ae5524f237c75832e4d8a9a1ae909228a3259ab
Nisarg Patel <nisargmpatel73@gmail.com>
2019-08-13 02:23:10 +0530

    DRF initialised

--- todo/urls.py
+++ todo/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.views.generic import TemplateView
+from todo import views
+from django.conf.urls import url
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='todo/index.html'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='todo/index.html')),
+    url(r'^todo/api/$', views.ToDoListView.as_view()),
 ]
