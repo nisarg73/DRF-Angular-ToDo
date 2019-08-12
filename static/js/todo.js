11bbc73fbc4d7531ccdd71cbe5936b3abaea3eeb
Nisarg Patel <nisargmpatel73@gmail.com>
2019-08-13 02:04:45 +0530

    Done functionality added

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,8 +1,17 @@
 var app = angular.module('toDo', []);
 app.controller('toDoController', function($scope){
-	$scope.todoList = [{todoText: 'Finish this tutorial'}]
-	$scope.todoAdd = function(){
-		$scope.todoList.push({todoText: $scope.todoInput});
-		$scope.todoInput = '';
-	};
+	$scope.todoList = [{todoText: 'Finish this tutorial', done: false}]
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText: $scope.todoInput, done: false});
+        $scope.todoInput = '';
+    };
+	$scope.remove = function() {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(todo) {
+            if (!todo.done) {
+                $scope.todoList.push(todo);
+            }
+        })
+    }
 })
